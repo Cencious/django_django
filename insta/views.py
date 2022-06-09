4196941f8ef1cec9730a2de1d5b101fe37426cb8
Cencious <user.email>
2022-06-10 01:35:54 +0300

    create app views

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,29 @@
+from poplib import POP3_SSL_PORT
 from django.shortcuts import render
 
-# Create your views here.
+
+posts =  [
+    {
+        'authors':'Inno',
+        'title':'Insta Post',
+        'content':'First post content',
+        'date_posted':'August 27,2021'
+
+    },
+    {
+        'authors':'Jane',
+        'title':'Insta Post 2',
+        'content':'Second post content',
+        'date_posted':'August 29,2021'
+
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request,'insta/home.html', context)
+
+def about(request):
+ return render(request,'insta/about.html')
