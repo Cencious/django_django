ee1fd07ba51e0143b2999e3e75a1aa1aeebca379
Cencious <user.email>
2022-06-10 01:36:54 +0300

    add  views

--- user/views.py
+++ user/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
+def register(request):
+      #put a check in place
+    if request.method=='POST':
+        form = UserCreationForm(request.POST)#create an instance
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('account')
+    else:
+        form = UserCreationForm(request.POST)
+    return render(request,'users/register.html',{'form':form})  
+
